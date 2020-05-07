cd5306e2f358f6758b54d940b6e992feb7c58e49
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-07 17:19:20 +0200

    bonus aggiunti

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function getRandom(min, max) {
 
 console.log(getRandom());
 for (var i = 0; i < 25; i++) {
-    $('.griglia').append('<div class="quadrato"></div>')
+    $('.griglia').append('<div class="quadrato"><span class="numero"></span></div>')
 }
 
-$( ".quadrato" ).each(function() {
-  $( this ).text(getRandom(0, 10));
+$( ".quadrato .numero" ).each(function() {
+  $(this).text(getRandom(0, 10));
+  console.log(parseInt($(this).text()));
+  if (parseInt($(this).text()) == 0) {
+    $(this).addClass('verde')
+  }
+  else if (parseInt($(this).text()) % 2 == 0) {
+    $(this).addClass('rosso')
+  }
 });
