5cfe9668f0a4e96b17ba94496ad2ed75f213fe8d
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-08 10:13:48 +0200

    tolti console.log e parseInt

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-console.log(getRandom());
 for (var i = 0; i < 25; i++) {
     $('.griglia').append('<div class="quadrato"><span class="numero"></span></div>')
 }
 
 $( ".quadrato .numero" ).each(function() {
   $(this).text(getRandom(0, 10));
-  console.log(parseInt($(this).text()));
-  if (parseInt($(this).text()) == 0) {
+  
+  if ($(this).text() == 0) {
     $(this).addClass('verde')
   }
-  else if (parseInt($(this).text()) % 2 == 0) {
+  else if ($(this).text() % 2 == 0) {
     $(this).addClass('rosso')
   }
 });
